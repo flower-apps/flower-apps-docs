1a748cd541bbc67338de7e381c18624f234c4a4a
bassem <bkhadige@gmail.com>
2023-03-19 18:27:54 +0100

    add - intro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Flower Apps',
   tagline: 'Flower Apps documentation',
-  url: 'https://demo.flower-apps.com',
+  url: 'https://docs.flower-apps.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'flower-apps', // Usually your GitHub org/user name.
   projectName: 'flower-apps-docs', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
